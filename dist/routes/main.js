f2e1047d1c39fd42a847e75beb5cab2545ff13d1
unknown <satvik.sharma@sourcefuse.com>
2021-07-21 16:47:44 +0530

    Create Query update

--- dist/routes/main.js
+++ dist/routes/main.js
@@ -1,28 +1,51 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var router = express_1.Router();
-var pg_1 = __importDefault(require("pg"));
-var conString = 'postgresql://admin:123456@localhost/user-db';
-var client = new pg_1.default.Client(conString);
+var body_parser_1 = require("body-parser");
+router.use(body_parser_1.json());
+var pg_1 = require("pg");
+// const conString = 'postgresql://admin:123456@localhost/user-db'
+var pool = new pg_1.Pool({
+    user: 'admin',
+    host: 'localhost',
+    database: 'user-db',
+    password: '123456',
+    port: 5432,
+});
 router.get("/", function (req, res) {
-    client.connect();
-    client.query('SELECT * FROM roles', function (err, result) {
+    var query = 'SELECT * FROM users';
+    pool.query(query, function (err, result) {
         if (err) {
             return console.error('error running query', err);
         }
         console.log(result.rows);
         res.render('index', { recipes: result.rows });
-        client.end();
     });
 });
-router.post("/new/:id", function (req, res) {
+router.post("/new", function (req, res) {
+    var query = "INSERT INTO users(\"first_name\", \"last_name\", \"phone\", \"email\", \"address\", \"role_id\", \"costumer_web\") VALUES( '" + req.body.first_name + "' , '" + req.body.last_name + "' ,'" + req.body.phone + "' , '" + req.body.email + "', '" + req.body.address + "' , '" + req.body.role_id + "' , '" + req.body.customer_web + "' )";
+    // console.log(req.body);
+    // console.log(`Hi, ${req.body.first_name}`);
+    console.log(req.body.first_name + ", " + req.body.last_name + "," + req.body.phone + ", " + req.body.address + ", " + req.body.role_id + ", " + req.body.customer_web);
+    pool.query(query, function (err, result) {
+        if (err) {
+            throw err;
+        }
+        res.send(result);
+    });
 });
 router.patch("/update/:id", function (req, res) {
 });
-router.delete("/delete/:id", function (req, res) {
+router.delete("/delete/:phone", function (req, res) {
+    // client.connect();
+    // client.query('DELETE FROM users WHERE phone = ${req.params.phone}', function(err, result) {
+    //     if(err) {
+    //      return console.error('error running query', err);
+    //     }
+    //     console.log(result.rows);
+    //     client.end();
+    //     res.redirect("/");
+    // }); 
 });
 exports.default = router;
--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,34 +1,58 @@
 import {Router} from "express";
 const router  = Router();
+import {json} from 'body-parser';
+router.use(json());
 
-import pg from "pg";
-const conString = 'postgresql://admin:123456@localhost/user-db'
 
-var client = new pg.Client(conString);
-
+import {Pool} from "pg";
+// const conString = 'postgresql://admin:123456@localhost/user-db'
+const pool = new Pool({
+    user: 'admin',
+    host: 'localhost',
+    database: 'user-db',
+    password: '123456',
+    port: 5432,
+})
 
 router.get("/", (req, res) => {
-    client.connect();
-    client.query('SELECT * FROM roles', function(err, result) {
+    const query = 'SELECT * FROM users';
+    pool.query(query, (err, result) => {
         if(err) {
          return console.error('error running query', err);
         }
         console.log(result.rows);
         res.render('index', {recipes: result.rows});
-        client.end();
     }); 
 });
 
-router.post("/new/:id", (req, res) => {
 
+router.post("/new", (req, res) => {
+    const query = `INSERT INTO users("first_name", "last_name", "phone", "email", "address", "role_id", "costumer_web") VALUES( '${req.body.first_name}' , '${req.body.last_name}' ,'${req.body.phone}' , '${req.body.email}', '${req.body.address}' , '${req.body.role_id}' , '${req.body.customer_web}' )`;
+    // console.log(req.body);
+    // console.log(`Hi, ${req.body.first_name}`);
+    console.log(`${req.body.first_name}, ${req.body.last_name},${req.body.phone}, ${req.body.address}, ${req.body.role_id}, ${req.body.customer_web}`);
+    pool.query(query, (err: any, result: any) => {
+        if (err) {
+            throw err;
+        }
+        res.send(result);
+    })
 });
 
 router.patch("/update/:id", (req, res) => {
 
 });
 
-router.delete("/delete/:id", (req, res) => {
-
+router.delete("/delete/:phone", (req, res) => {
+    // client.connect();
+    // client.query('DELETE FROM users WHERE phone = ${req.params.phone}', function(err, result) {
+    //     if(err) {
+    //      return console.error('error running query', err);
+    //     }
+    //     console.log(result.rows);
+    //     client.end();
+    //     res.redirect("/");
+    // }); 
 });
 
 
