6dc1885a221d16d94e0e262eaf9c74b48533e5f3
unknown <satvik.sharma@sourcefuse.com>
2021-07-21 15:43:22 +0530

    DB Setup

--- dist/routes/main.js
+++ dist/routes/main.js
@@ -1,12 +1,28 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var router = express_1.Router();
+var pg_1 = __importDefault(require("pg"));
+var conString = 'postgresql://admin:123456@localhost/user-db';
+var client = new pg_1.default.Client(conString);
 router.get("/", function (req, res) {
-    res.render('index');
+    client.connect();
+    client.query('SELECT * FROM roles', function (err, result) {
+        if (err) {
+            return console.error('error running query', err);
+        }
+        console.log(result.rows);
+        res.render('index', { recipes: result.rows });
+        client.end();
+    });
 });
-router.post("/:id", function (req, res) {
+router.post("/new/:id", function (req, res) {
 });
-router.delete("/:id", function (req, res) {
+router.patch("/update/:id", function (req, res) {
+});
+router.delete("/delete/:id", function (req, res) {
 });
 exports.default = router;
--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,16 +1,33 @@
 import {Router} from "express";
 const router  = Router();
 
+import pg from "pg";
+const conString = 'postgresql://admin:123456@localhost/user-db'
+
+var client = new pg.Client(conString);
+
 
 router.get("/", (req, res) => {
-    res.render('index');
+    client.connect();
+    client.query('SELECT * FROM roles', function(err, result) {
+        if(err) {
+         return console.error('error running query', err);
+        }
+        console.log(result.rows);
+        res.render('index', {recipes: result.rows});
+        client.end();
+    }); 
 });
 
-router.post("/:id", (req, res) => {
+router.post("/new/:id", (req, res) => {
 
 });
 
-router.delete("/:id", (req, res) => {
+router.patch("/update/:id", (req, res) => {
+
+});
+
+router.delete("/delete/:id", (req, res) => {
 
 });
 
